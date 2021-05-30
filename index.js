e0751186243fcf377a8cbd85b8a60ef0b0810d58
Andrew Masih <armasih@outlook.com>
2021-05-30 22:55:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/*selects in first subArray last element because first Array is at 'index 0' and c letter in the first Array is at 'index 2' */
+
+
+let twoDimensionalArray = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]];
+
+console.log(twoDimensionalArray);
+console.log(twoDimensionalArray[0][2]);
